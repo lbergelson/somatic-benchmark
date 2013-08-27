6fee62282874d7b3ca284e89c16f9e207391f342
Louis Bergelson <louisb@broadinstitute.org>
2013-08-27 12:01:13 -0400

    Modified RunBenchmark to read in the bamTypes file.

--- RunBenchmark.scala
+++ RunBenchmark.scala
@@ -1,49 +1,38 @@
 package org.broadinstitute.cga.benchmark.queue
 
 import org.broadinstitute.sting.queue.QScript
-import org.broadinstitute.sting.utils.io.FileExtension
+import org.broadinstitute.sting.utils.io.{IOUtils, FileExtension}
 import org.broadinstitute.sting.queue.function.RetryMemoryLimit
 import org.broadinstitute.sting.utils.exceptions.UserException.CouldNotReadInputFile
+import java.io.{IOException, FileReader}
+import scala.io.Source
+import org.apache.commons.io.IOUtils
 
 class RunBenchmark extends QScript {
   qscript =>
 
-  @Argument(doc="If this is set than only 1 set of files will be run instead of the complete array.", required=false)
-  var is_test = false
-
+//  @Argument(doc="If this is set than only 1 set of files will be run instead of the complete array.", required=false)
+//  var is_test = false
 
   @Argument(fullName="tool", shortName="t", doc="The name of a tool to run.  A matching script named run<Tool>.sh must be placed in the tool-scripts directory.")
   var tool_names: List[String]  = Nil
 
-  lazy val ALLELE_FRACTIONS = if(is_test) List(0.8) else List(0.04, 0.1, 0.2, 0.4, 0.8)
-  lazy val TUMOR_DEPTHS = if(is_test) List("123456789ABC") else List("123456789ABC",
-                                                               "123456789AB",
-                                                               "123456789A",
-                                                               "123456789",
-                                                               "12345678",
-                                                               "1234567",
-                                                               "123456",
-                                                               "12345",
-                                                               "1234",
-                                                               "123",
-                                                               "12",
-                                                               "1")
-
-
-  val GERMLINE_NAME_TEMPLATE = "NA12878.somatic.simulation.merged.%s.bam"
   val GERMLINE_MIX_DIR = new File("data_1g_wgs")
-
-  def germlineMixFile(abrv :String) = AbrvFile.fromTemplate(GERMLINE_MIX_DIR, GERMLINE_NAME_TEMPLATE, abrv)
-
-  lazy val FP_NORMAL_DEPTHS = (if (is_test) List("DEFGHI") else List("D", "DE", "DEF", "DEFG", "DEFGH","DEFGHI", "FG", "HI")).map(germlineMixFile )
-  val SPIKE_NORMAL_DEPTHS = List("DEFGHI").map(germlineMixFile )
-
-
   val SPIKE_DIR = new File("fn_data")
 
+
+  val bamTypesFile : File = new File("bams.bamtype")
   val referenceFile : File = new File("/home/unix/louisb/cga_home/reference/human_g1k_v37_decoy.fasta")
 
+  lazy val (normals,tumors,spikedTumors) = readBamTypesFile(bamTypesFile)
+  lazy val spikedNormal = normals.sortBy(_.length).last
+
+
+
+
   def script() {
+
+
     val tools = getTools(tool_names)
     val (outFPDirs, fpCmds) = getFalsePositiveCommands(tools).unzip
     val (outSpikedDirs, spikeCmds ) = getSpikedCommands(tools).unzip
@@ -64,13 +53,6 @@ class RunBenchmark extends QScript {
     def withPath(path: String) = new AbrvFile(path, abrv)
   }
 
-  object AbrvFile{
-    def fromTemplate(dir: File, nameTemplate: String, abrv: String) = {
-        val name = nameTemplate.format(abrv)
-        val file = new File(dir,name)
-        new AbrvFile(file, abrv)
-    }
-  }
 
          //invokes <tool> with parameters <normal><tumor><reference><outputDir>
   class ToolInvocation extends  CommandLineFunction with RetryMemoryLimit{
@@ -109,43 +91,25 @@ class RunBenchmark extends QScript {
     }
   }
 
-  def getFalsePositiveCommands(tools: List[AbrvFile]) = {
-    def getPureFalsePositivePairs(normals: List[AbrvFile], tumors: List[AbrvFile]) = {
+  def getFalsePositiveCommands(tools: Traversable[AbrvFile]) = {
+    def getPureFalsePositivePairs(normals: Traversable[AbrvFile], tumors: Traversable[AbrvFile]) = {
         for{
             normal <- normals
             tumor <- tumors
         } yield (normal, tumor)
     }
 
-    val pureGermline = getPureFalsePositivePairs(FP_NORMAL_DEPTHS, TUMOR_DEPTHS.map(germlineMixFile) )
+    val pureGermline = getPureFalsePositivePairs(normals, tumors )
     generateCmds(tools, pureGermline, "germline_mix")
  }
 
  def getSpikedCommands(tools:List[AbrvFile]) = {
-     def getSomaticSpikedPairs(normals: List[AbrvFile], alleleFraction: List[Double], depths: List[String]) = {
-        def tumorFile(fraction: Double, depth: String): AbrvFile = {
-            val tumorName = "NA12878_%s_NA12891_%s_spikein.bam".format(depth, fraction)
-            val tumorFile = new File(SPIKE_DIR, tumorName)
-            val abrv = "%s_%s".format(depth,fraction)
-            new AbrvFile(tumorFile, abrv)
-        }
-
-        for{
-            normal <- normals
-            fraction <- alleleFraction
-            depth <- depths
-        } yield {
-            val tumor = tumorFile(fraction, depth)
-            (normal, tumor)
-        }
-    }
-
-    val spiked = getSomaticSpikedPairs(SPIKE_NORMAL_DEPTHS, ALLELE_FRACTIONS, TUMOR_DEPTHS)
+    val spiked = spikedTumors.map(tumor => (spikedNormal, tumor))
     generateCmds(tools, spiked, "spiked")
  }
 
 
-  def generateCmds(toolsToTest: List[AbrvFile], normalTumorPairs: List[(AbrvFile, AbrvFile)], outputDir: File):List[(File, CommandLineFunction)] = {
+  def generateCmds(toolsToTest: Traversable[AbrvFile], normalTumorPairs: Traversable[(AbrvFile, AbrvFile)], outputDir: File):Traversable[(File, CommandLineFunction)] = {
     def generateCmd(tool: AbrvFile, normal:AbrvFile, tumor: AbrvFile, outputDir: File): (File, CommandLineFunction) ={
         val individualOutputDir = new File(outputDir, "%s_N%S_T%S".format(tool.abrv, normal.abrv, tumor.abrv))
         (individualOutputDir, ToolInvocation(tool=tool, normal=normal, tumor=tumor, reference=referenceFile, outputDir=individualOutputDir) )
@@ -155,6 +119,35 @@ class RunBenchmark extends QScript {
      tool <- toolsToTest
      (normal, tumor) <- normalTumorPairs
     } yield generateCmd(tool, normal, tumor, outputDir)
+  }
+
+  def readBamTypesFile(typesFile: File) = {
+    try{
+
+        val source = Source.fromFile(typesFile)
+
+        val lines = source.getLines()
+        val splits = lines.map(_.split('\t')).map(splitLine => (splitLine(0), splitLine(1))).toList
+
+        def newGermlineMixFile(name : String)= {
+            new AbrvFile(new File(GERMLINE_MIX_DIR, name), name.split('.')(1) )
+        }
+        def newSpikedFile(name : String) = {
+            new AbrvFile(new File(SPIKE_DIR, name), name.split('_')(1))
+        }
+
+        val tumor = splits.collect  { case ("TUMOR", name) => newGermlineMixFile(name)  }
+        val normal = splits.collect { case ("NORMAL", name) => newGermlineMixFile(name) }
+        val spiked = splits.collect { case ("SPIKED", name) => newSpikedFile(name) }
+
+        logger.debug("Normal: "+normal)
+        logger.debug("Tumor: "+tumor)
+        logger.debug("Spiked: " + spiked)
+
+        (normal, tumor, spiked)
+    } catch {
+        case e: IOException => throw new CouldNotReadInputFile(typesFile, "Could not read bam types file", e)
+    }
   }
 
 }
