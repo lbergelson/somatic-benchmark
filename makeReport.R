f294790afe6317d8b077d637db76b517de08bc22
Louis Bergelson <louisb@broadinstitute.org>
2014-02-14 17:16:50 -0500

    Including the report generator.

---
+++ makeReport.R
@@ -0,0 +1,65 @@
+library(Nozzle.R1)
+library(plyr)
+dir.create( "reports", showWarnings=FALSE );
+
+args <- commandArgs(trailingOnly=TRUE)
+print(args)
+
+stats_file = args[1] 
+
+stats <- read.delim(stats_file)
+
+stats_summary <- ddply(stats, .(EvaluationGroup, Caller,Version,Time),
+	 function(df) c(fp_snp = mean(df$fp_snp), tp_snp = mean(df$tp_snp), fn_snp = mean(df$fn_snp), novel_snp = mean(df$novel_snp),
+		fp_indel = mean(df$fp_indel), tp_indel = mean(df$tp_indel), fn_indel = mean(df$fn_indel), novel_indel = mean(df$novel_indel) ))
+stats_summary <- mutate(stats_summary, sensitivity_snp = tp_snp/(fp_snp+tp_snp))
+stats_summary <- mutate(stats_summary, sensitivity_indel = tp_indel/(fp_indel+tp_indel))
+
+stats_summary <- mutate(stats_summary, specificity_snp = tp_snp/(fp_snp+tp_snp))
+stats_summary <- mutate(stats_summary, specificity_indel = tp_indel/(fp_indel+tp_indel))
+
+
+
+
+# Phase 1: create report elements
+r <- newCustomReport( "Caller LeaderBoard")
+hcc1143 <- newSection( "HCC1143" );
+hcc1954 <- newSection( "HCC1954" );
+normalNormal <- newSection( "NormalNormal");
+completeData <- newSection( "Complete");
+
+common_cols <- c("Caller", "Version", "Time")
+snp_cols = c(common_cols,c("tp_snp","fp_snp","fn_snp","novel_snp","sensitivity_snp","specificity_snp"))
+indel_cols = c(common_cols, c("tp_indel","fp_indel","fn_indel","novel_indel","sensitivity_indel","sensitivity_indel"))
+print(snp_cols)
+print(indel_cols)
+
+stat_1143_s <- stats_summary[stats_summary$EvaluationGroup == "HCC1143",snp_cols]
+stat_1143_i <- stats_summary[stats_summary$EvaluationGroup == "HCC1143",indel_cols]
+
+stat_1954_s <- stats_summary[stats_summary$EvaluationGroup == "HCC1954",snp_cols]
+stat_1954_i <- stats_summary[stats_summary$EvaluationGroup == "HCC1954",indel_cols]
+
+stat_nn <- stats_summary[stats_summary$EvaluationGroup =="NormalNormal", c(common_cols, c("fp_snp", "fp_indel"))]
+
+
+t_1143_s <- newTable( stat_1143_s, "HCC1143 Snps" ); # w/ caption
+t_1143_i <- newTable( stat_1143_i, "HCC1143 Indels" ); # w/ caption
+
+t_1954_s <- newTable( stat_1954_s, "HCC1954 Snps" );
+t_1954_i <- newTable( stat_1954_i, "HCC1954 Indels" );
+
+t_nn <- newTable(stat_nn, "Normal-Normal Stats");
+
+t_complete <- newTable(stats, "Complete unaggregated calls")
+
+# Phase 2: assemble report structure bottom-up
+hcc1143 <- addTo(hcc1143, t_1143_s,t_1143_i)
+hcc1954 <- addTo(hcc1954, t_1954_s,t_1954_i)
+normalNormal <- addTo(normalNormal, t_nn)
+completeData <- addTo(completeData, t_complete)
+
+r <- addTo(r, hcc1143, hcc1954, normalNormal, completeData)
+
+# Phase 3: render report to file
+writeReport( r, filename="reports/my_report" ); # w/o extension
